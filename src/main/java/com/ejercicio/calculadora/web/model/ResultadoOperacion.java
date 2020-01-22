8cebc16117e762d33b22555dae9d7f8eabf5e804
josemar7 <josemar7@gmail.com>
2020-01-22 14:29:13 +0100

    lombok

--- src/main/java/com/ejercicio/calculadora/web/model/ResultadoOperacion.java
+++ src/main/java/com/ejercicio/calculadora/web/model/ResultadoOperacion.java
@@ -1,60 +1,21 @@
 package com.ejercicio.calculadora.web.model;
 
-import java.math.BigDecimal;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ResultadoOperacion {
 
-    BigDecimal operando1;
-    BigDecimal operando2;
-    String operacion;
-    BigDecimal resultado;
+    private BigDecimal operando1;
+    private BigDecimal operando2;
+    private String operacion;
+    private BigDecimal resultado;
 
-    public BigDecimal getOperando1() {
-        return operando1;
-    }
-
-    public void setOperando1(BigDecimal operando1) {
-        this.operando1 = operando1;
-    }
-
-    public BigDecimal getOperando2() {
-        return operando2;
-    }
-
-    public void setOperando2(BigDecimal operando2) {
-        this.operando2 = operando2;
-    }
-
-    public String getOperacion() {
-        return operacion;
-    }
-
-    public void setOperacion(String operacion) {
-        this.operacion = operacion;
-    }
-
-    public BigDecimal getResultado() {
-        return resultado;
-    }
-
-    public void setResultado(BigDecimal resultado) {
-        this.resultado = resultado;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ResultadoOperacion that = (ResultadoOperacion) o;
-        return operando1.equals(that.operando1) &&
-                operando2.equals(that.operando2) &&
-                operacion.equals(that.operacion) &&
-                resultado.equals(that.resultado);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(operando1, operando2, operacion, resultado);
-    }
 }
